d9f92cf6bc684c95880e8fc054271d8e83120998
Bruno <brugacag@gmail.com>
2024-02-29 12:36:44 -0300

    Initial commit.

---
+++ src/main/java/dev/android/bruno/meuappstreaming/MainActivity.kt
@@ -0,0 +1,16 @@
+package dev.android.bruno.meuappstreaming
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun sairLoja(view: View){
+        finish()
+    }
+}
